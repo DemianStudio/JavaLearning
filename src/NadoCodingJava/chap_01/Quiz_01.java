5d1f92fb02383a1c8a0dacf018fd35f100eabfce
demian <rudejrqkr97@gmail.com>
2023-10-14 17:41:14 +0900

    :rocket: 나도코딩 챕터1_Quiz.01

--- src/NadoCodingJava/chap_01/Quiz_01.java
+++ src/NadoCodingJava/chap_01/Quiz_01.java
@@ -10,17 +10,13 @@ public class Quiz_01 {
         System.out.println("버스 번호는 '1234', '상암08'과 같은 형태");
         System.out.println("남은 시간은 분 단위 (예: 3분, 5분)");
         System.out.println("남은 거리는 km단위 (예: 1.5km, 0.8km)");
-
-        System.out.println("==========");
-        // 버스 번호
-        int busNum = 1234;
-
-        // 남은 시간
-        String limitTime = "5분";
-
-        // 남은 거리(km)
-        double limitStreet = 1.5;
-
+        System.out.println();
+        System.out.println("=== 결과 ===");
+        // 풀이
+        int busNum = 1234; // 버스 번호
+        String limitTime = "5분"; // 남은 시간
+        double limitStreet = 1.5; // 남은 거리(km)
+        // 결과
         System.out.println("버스 번호는 " + busNum + "버스");
         System.out.println("약 " + limitTime + " 후 도착");
         System.out.println("남은 거리는 " + limitStreet + "km");
