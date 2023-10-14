858aaa29a9c95182f9f93df0ee9c8c0c671f9cb4
demian <rudejrqkr97@gmail.com>
2023-10-14 22:31:03 +0900

    :rocket: 나도코딩 챕터3_StringCompare

---
+++ src/NadoCodingJava/chap_03/StringCompare.java
@@ -0,0 +1,30 @@
+package NadoCodingJava.chap_03;
+
+public class StringCompare {
+    public static void main(String[] args) {
+        System.out.println("=== 문자열 비교 ===");
+        // 문자열 비교
+        String s1 = "Java";
+        String s2 = "Python";
+        System.out.println(s1.equals(s2)); // 문자열 내용이 같으면 true, 다르면 false
+        System.out.println(s1.equals("Java"));
+        System.out.println(s2.equals("python"));
+        System.out.println(s2.equalsIgnoreCase("python")); // 대소문자 구분 없이 문자열 내용이 같은지 여부 체크
+
+        System.out.println();
+
+        // 문자열 비교 심화
+        System.out.println("=== 문자열 비교 심화 ===");
+        // 하나의 메모지
+        s1 = "1234";
+        s2 = "1234";
+        System.out.println(s1.equals(s2)); // true (내용)
+        System.out.println(s1 == s2); // true (침조)
+
+        // 각각의 메모지
+        s1 = new String("1234");
+        s2 = new String("1234");
+        System.out.println(s1.equals(s2)); // ture
+        System.out.println(s1 == s2); // false
+    }
+}
